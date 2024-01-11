71d37eba44081e5bbd145ceaa84a12c20f8d81f4
JGrant024 <jonathan.grant23@icloud.com>
2024-01-11 14:44:11 -0500

    create files for python exercises

---
+++ nested_dictionaries.py
@@ -0,0 +1,26 @@
+# Given the following dictionary:
+
+# ramit = {
+#     'name': 'Ramit',
+#     'email': 'ramit@gmail.com',
+#     'interests': ['movies', 'tennis'],
+#     'friends': [
+#         {
+#         'name': 'Jasmine',
+#         'email': 'jasmine@yahoo.com',
+#         'interests': ['photography', 'tennis']
+#         },
+#         {
+#         'name': 'Jan',
+#         'email': 'jan@hotmail.com',
+#         'interests': ['movies', 'tv']
+#         }
+#     ]
+# }
+
+# Write a python expression that gets the email address of Ramit.
+# Write a python expression that gets the first of Ramit's interests.
+# Write a python expression that gets the email address of Jasmine.
+# Write a python expression that gets the second of Jan's two interests.
+# NOTE: "Python expression" just means something like print(dictionary[key]). Although, in two of the above, using a for loop will be helpfu!
+
---
+++ word_histogram.py
@@ -0,0 +1,5 @@
+# Write a word_histogram program that asks the user for a sentence as its input, and prints a dictionary containing the tally of how many times each word in the alphabet was used in the text. For example:
+
+# $ python3 word_histogram.py
+# Please enter a sentence: To be or not to be
+# {'to': 2, 'be': 2, 'or': 1, 'not': 1}
