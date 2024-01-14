81056332ef68ae6f79530d8a8158b1ad60c66c02
JGrant024 <jonathan.grant23@icloud.com>
2024-01-13 23:21:05 -0500

    updated level_9000.py folder

---
+++ level_9000.py
@@ -0,0 +1,104 @@
+# Phone Book App
+
+
+# You will write a command line program to manage a phone book. When you start the phonebook.py program, it will print out a menu and ask the user to enter a choice:
+
+# $ python3 phonebook.py
+# Electronic Phone Book
+# =====================
+# 1. Look up an entry
+# 2. Set an entry
+# 3. Delete an entry
+# 4. List all entries
+# 5. Quit
+
+# What do you want to do (1-5)?
+
+# If they choose to look up an entry, you will ask them for the person's name, and then look up the person's phone number by the given name and print it to the screen.
+# If they choose to set an entry, you will prompt them for the person's name and the person's phone number,
+# If they choose to delete an entry, you will prompt them for the person's name and delete the given person's entry.
+# If they choose to list all entries, you will go through all entries in the dictionary and print each out to the terminal.
+# If they choose to quit, end the program.
+# Example session:
+
+# $ python3 phonebook.py
+
+# Electronic Phone Book
+# =====================
+# 1. Look up an entry
+# 2. Set an entry
+# 3. Delete an entry
+# 4. List all entries
+# 5. Quit
+# What do you want to do (1-5)? 2
+
+# Name: Melissa
+# Phone Number: 584-394-5857
+# Entry stored for Melissa.
+
+# Electronic Phone Book
+# =====================
+# 1. Look up an entry
+# 2. Set an entry
+# 3. Delete an entry
+# 4. List all entries
+# 5. Quit What do you want to do (1-5)? 2
+
+# Name: Igor
+# Phone Number: 857-485-2935
+# Entry stored for Igor.
+
+# Electronic Phone Book
+# =====================
+# 1. Look up an entry
+# 2. Set an entry
+# 3. Delete an entry
+# 4. List all entries
+# 5. Quit What do you want to do (1-5)? 2
+
+# Name: Jazz
+# Phone Number: 334-584-2345
+# Entry stored for Jazz.
+
+# Electronic Phone Book
+# =====================
+# 1. Look up an entry
+# 2. Set an entry
+# 3. Delete an entry
+# 4. List all entries
+# 5. Quit
+# What do you want to do (1-5)? 1
+
+# Name: Melissa
+# Found entry for Melissa: 584-394-5857
+
+# Electronic Phone Book
+# =====================
+# 1. Look up an entry
+# 2. Set an entry
+# 3. Delete an entry
+# 4. List all entries
+# 5. Quit What do you want to do (1-5)? 3
+
+# Name: Melissa
+# Deleted entry for Melissa
+
+# Electronic Phone Book
+# =====================
+# 1. Look up an entry
+# 2. Set an entry
+# 3. Delete an entry
+# 4. List all entries
+# 5. Quit What do you want to do (1-5)? 4
+
+# Found entry for Igor: 857-485-2935
+# Found entry for Jazz: 334-584-2345
+
+# Electronic Phone Book =====================
+# 1. Look up an entry
+# 2. Set an entry
+# 3. Delete an entry
+# 4. List all entries
+# 5. Quit What do you want to do (1-5)? 5
+
+# Bye.
