a9caf7029d2372af53a688def206d229afdde30b
JGrant024 <jonathan.grant23@icloud.com>
2024-01-15 02:58:56 -0500

    updated .py file friend counter

--- friend_counter.py
+++ friend_counter.py
@@ -1,3 +1,13 @@
 # Using the dictionary from the Nested dictionaries exercise above, 
 # write a function countFriends() that accepts a dictionary as an argument 
 # and returns a new dictionary that includes a new key friends_count:
+
+
+def countFriends(ramit): 
+    for key in ramit: 
+        print("key:", key, "value:" [key])
+
+
+
+ramit = {'a':1, 'b':2, 'c':3} 
+countFriends(ramit)
