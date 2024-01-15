d1a8b2ddff8dbaaea97186c44717182fcd072df6
JGrant024 <jonathan.grant23@icloud.com>
2024-01-15 02:40:17 -0500

    updated python excercises and corrected files

--- friend_counter.py
+++ friend_counter.py
@@ -1,6 +1,3 @@
-# # Using the dictionary from the Nested dictionaries exercise above, write a function countFriends() that accepts a dictionary as an argument and returns a new dictionary that includes a new key friends_count:
-# Write a letter_histogram program that asks the user for input, and prints a dictionary containing the tally of how many times each letter in the alphabet was used in the word. For example:
-
-# $ python3 letter_histogram.py
-# Please enter a word: banana
-# {'a': 3, 'b': 1, 'n': 2}
+# Using the dictionary from the Nested dictionaries exercise above, 
+# write a function countFriends() that accepts a dictionary as an argument 
+# and returns a new dictionary that includes a new key friends_count:
---
+++ letter_summary.py
@@ -0,0 +1,7 @@
+# Letter Summary
+
+# Write a letter_histogram program that asks the user for input, and prints a dictionary containing the tally of how many times each letter in the alphabet was used in the word. For example:
+
+# $ python3 letter_histogram.py
+# Please enter a word: banana
+# {'a': 3, 'b': 1, 'n': 2}
